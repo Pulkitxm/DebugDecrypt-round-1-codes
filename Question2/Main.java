26941d897dc9c9640c67bff84621bca6254b8370
Pulkit <kpulkit15234@gmail.com>
2024-04-03 21:59:53 +0530

    added q2

---
+++ Question2/Main.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sadhfsdfhvbuisdfhbihusdfghbuisdfghuisdrfghsdilufghnsidufghnsdilfghnslidufghjn = new Scanner(System.in);
+        int fjdkasssssssss = sadhfsdfhvbuisdfhbihusdfghbuisdfghuisdrfghsdilufghnsidufghnsdilfghnslidufghjn.nextInt();
+        int[] ek_kao_hota_hai_do_ka_ten_hota_hai_char_ath = new int[fjdkasssssssss];
+        for (int i = 0; i < fjdkasssssssss; i++) {
+            ek_kao_hota_hai_do_ka_ten_hota_hai_char_ath[i] = sadhfsdfhvbuisdfhbihusdfghbuisdfghuisdrfghsdilufghnsidufghnsdilfghnslidufghjn.nextInt();
+        }
+        int qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqww12 = sadhfsdfhvbuisdfhbihusdfghbuisdfghuisdrfghsdilufghnsidufghnsdilfghnslidufghjn.nextInt();
+        System.out.println(______________________________________________________________babu_moshai_zindagi_badi_honi_chahiye_lambi_nahi______________________________________________________________(ek_kao_hota_hai_do_ka_ten_hota_hai_char_ath, qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqww12));
+        sadhfsdfhvbuisdfhbihusdfghbuisdfghuisdrfghsdilufghnsidufghnsdilfghnslidufghjn.close();
+    }
+
+    public static void ______________________________________________________________babu_moshai_zindagi_badi_honi_chahiye_lambi_nahi______________________________________________________________(int[] ikiaaaaaa) {
+        int bbasjchdkhbasdjcvhbnasiudfvhbnasdilufvhbnasdiufgvnhbsadiufvhnasouiedfgvhnedwsfoigjuhn = 0;
+        int sfhjdfsjghdfiujghsdyhujfghbyuisdfhgiusefhuiesrhiuwerfheriuhegrfhsdfvhjksfgdhjkfgdshjkfsdghjksfdhkjfsdhjkfsdhjksdfghjksdfghjksgdfhjkgeriuherfgiurtghsediuersghiuwertghweiurghweiurthewirughwerighsergiuhewrfipuewrhfgweiruhwergfipwertghweriuhwefi9pweiurthfwergiuhewrfwerliuhwergiuwerhfgewrgidxfbnvsdfjkhvn = ikiaaaaaa.length - 1;
+        while (bbasjchdkhbasdjcvhbnasiudfvhbnasdilufvhbnasdiufgvnhbsadiufvhnasouiedfgvhnedwsfoigjuhn < sfhjdfsjghdfiujghsdyhujfghbyuisdfhgiusefhuiesrhiuwerfheriuhegrfhsdfvhjksfgdhjkfgdshjkfsdghjksfdhkjfsdhjkfsdhjksdfghjksdfghjksgdfhjkgeriuherfgiurtghsediuersghiuwertghweiurghweiurthewirughwerighsergiuhewrfipuewrhfgweiruhwergfipwertghweriuhwefi9pweiurthfwergiuhewrfwerliuhwergiuwerhfgewrgidxfbnvsdfjkhvn) {
+            int temp = ikiaaaaaa[bbasjchdkhbasdjcvhbnasiudfvhbnasdilufvhbnasdiufgvnhbsadiufvhnasouiedfgvhnedwsfoigjuhn];
+            ikiaaaaaa[bbasjchdkhbasdjcvhbnasiudfvhbnasdilufvhbnasdiufgvnhbsadiufvhnasouiedfgvhnedwsfoigjuhn] = ikiaaaaaa[sfhjdfsjghdfiujghsdyhujfghbyuisdfhgiusefhuiesrhiuwerfheriuhegrfhsdfvhjksfgdhjkfgdshjkfsdghjksfdhkjfsdhjkfsdhjksdfghjksdfghjksgdfhjkgeriuherfgiurtghsediuersghiuwertghweiurghweiurthewirughwerighsergiuhewrfipuewrhfgweiruhwergfipwertghweriuhwefi9pweiurthfwergiuhewrfwerliuhwergiuwerhfgewrgidxfbnvsdfjkhvn];
+            ikiaaaaaa[sfhjdfsjghdfiujghsdyhujfghbyuisdfhgiusefhuiesrhiuwerfheriuhegrfhsdfvhjksfgdhjkfgdshjkfsdghjksfdhkjfsdhjkfsdhjksdfghjksdfghjksgdfhjkgeriuherfgiurtghsediuersghiuwertghweiurghweiurthewirughwerighsergiuhewrfipuewrhfgweiruhwergfipwertghweriuhwefi9pweiurthfwergiuhewrfwerliuhwergiuwerhfgewrgidxfbnvsdfjkhvn] = temp;
+            bbasjchdkhbasdjcvhbnasiudfvhbnasdilufvhbnasdiufgvnhbsadiufvhnasouiedfgvhnedwsfoigjuhn++;
+            sfhjdfsjghdfiujghsdyhujfghbyuisdfhgiusefhuiesrhiuwerfheriuhegrfhsdfvhjksfgdhjkfgdshjkfsdghjksfdhkjfsdhjkfsdhjksdfghjksdfghjksgdfhjkgeriuherfgiurtghsediuersghiuwertghweiurghweiurthewirughwerighsergiuhewrfipuewrhfgweiruhwergfipwertghweriuhwefi9pweiurthfwergiuhewrfwerliuhwergiuwerhfgewrgidxfbnvsdfjkhvn--;
+        }
+    }
+
+    public static int _____________________________________________________tum_nahi_samjhogi_anjali_kuch_kuch_hota_hai(int[] ek_do_ten_char_pach_chei_sath_ath_no_das_gyara, int dsgdfghdfghdfgh) {
+        return ______________________________________________________________babu_moshai_zindagi_badi_honi_chahiye_lambi_nahi______________________________________________________________beta_tumse_naa_ho_payega______________________________________________________________babu_moshai_zindagi_badi_honi_chahiye_lambi_nahi______________________________________________________________(ek_do_ten_char_pach_chei_sath_ath_no_das_gyara, dsgdfghdfghdfgh) - ______________________________________________________________babu_moshai_zindagi_badi_honi_chahiye_lambi_nahi______________________________________________________________beta_tumse_naa_ho_payega______________________________________________________________babu_moshai_zindagi_badi_honi_chahiye_lambi_nahi______________________________________________________________(ek_do_ten_char_pach_chei_sath_ath_no_das_gyara, dsgdfghdfghdfgh - 1);
+    }
+
+    public static int[] abra_ka_dabr_gili_gili_chu(int[] mera_nam_marry_hai_marry_sau_takka_teri_hai, int[] vndfkjlbvnsdkjfbhnsdfkj) {
+        int[] mei_hu_jadugar_mera_jadu_hai_kamal = new int[mera_nam_marry_hai_marry_sau_takka_teri_hai.length + vndfkjlbvnsdkjfbhnsdfkj.length];
+        int dfghdfghdfhgdfghdfghdfghdfghdfgh = 0, dfg = 0, qqqqqqs = 0;
+        while (dfghdfghdfhgdfghdfghdfghdfghdfgh < mera_nam_marry_hai_marry_sau_takka_teri_hai.length && dfg < vndfkjlbvnsdkjfbhnsdfkj.length) {
+            if (mera_nam_marry_hai_marry_sau_takka_teri_hai[dfghdfghdfhgdfghdfghdfghdfghdfgh] < vndfkjlbvnsdkjfbhnsdfkj[dfg]) {
+                mei_hu_jadugar_mera_jadu_hai_kamal[qqqqqqs++] = mera_nam_marry_hai_marry_sau_takka_teri_hai[dfghdfghdfhgdfghdfghdfghdfghdfgh++];
+            } else {
+                mei_hu_jadugar_mera_jadu_hai_kamal[qqqqqqs++] = vndfkjlbvnsdkjfbhnsdfkj[dfg++];
+            }
+        }
+        while (dfghdfghdfhgdfghdfghdfghdfghdfgh < mera_nam_marry_hai_marry_sau_takka_teri_hai.length) {
+            mei_hu_jadugar_mera_jadu_hai_kamal[qqqqqqs++] = mera_nam_marry_hai_marry_sau_takka_teri_hai[dfghdfghdfhgdfghdfghdfghdfghdfgh++];
+        }
+        while (qqqqqqs < vndfkjlbvnsdkjfbhnsdfkj.length) {
+            mei_hu_jadugar_mera_jadu_hai_kamal[qqqqqqs++] = vndfkjlbvnsdkjfbhnsdfkj[dfg++];
+        }
+        return mei_hu_jadugar_mera_jadu_hai_kamal;
+    }
+
+    public static int ______________________________________________________________babu_moshai_zindagi_badi_honi_chahiye_lambi_nahi______________________________________________________________beta_tumse_naa_ho_payega______________________________________________________________babu_moshai_zindagi_badi_honi_chahiye_lambi_nahi______________________________________________________________(int[] FGGGGGGGGGGFGFGFGFSAJDHFGJAYHSFGJAHKSDFGHJASDFGJAHSDFGASDJHFHGBASDFJGHBASDFKJGBASDFKYGBVASDF, int aazjxhdfvgbsdhjfghbsdjhfghbsdjhfgghbsdfgjhgbasdflighjxzdgfvbyhksujdgfasyefgasyedfgasyujdfgasujdyfgasdkjfygvbasdfjkhbgvasdfjkgvbasedujfhbgvawsdfljfhbvawsefljhbgasdflihasdgbfliuasdfgSADLJFHJGBASDFLJHGBZSADFLIJHUGBASDFLJHASDFLUJIAYHSDFLUJGHJGHASDFJHASDFHJ) {
+        if (aazjxhdfvgbsdhjfghbsdjhfghbsdjhfgghbsdfgjhgbasdflighjxzdgfvbyhksujdgfasyefgasyedfgasyujdfgasujdyfgasdkjfygvbasdfjkhbgvasdfjkgvbasedujfhbgvawsdfljfhbvawsefljhbgasdflihasdgbfliuasdfgSADLJFHJGBASDFLJHGBZSADFLIJHUGBASDFLJHASDFLUJIAYHSDFLUJGHJGHASDFJHASDFHJ == 0) return 0;
+        
+        int SDFGSDFGSDFG = 0;
+        int j = 0;
+        int res = 0;
+        int n = FGGGGGGGGGGFGFGFGFSAJDHFGJAYHSFGJAHKSDFGHJASDFGJAHSDFGASDJHFHGBASDFJGHBASDFKJGBASDFKYGBVASDF.length;
+        int[] map = new int[n + 1];
+        int distinctCount = 0;
+
+        while (j < n) {
+            map[FGGGGGGGGGGFGFGFGFSAJDHFGJAYHSFGJAHKSDFGHJASDFGJAHSDFGASDJHFHGBASDFJGHBASDFKJGBASDFKYGBVASDF[j]]++;
+            if (map[FGGGGGGGGGGFGFGFGFSAJDHFGJAYHSFGJAHKSDFGHJASDFGJAHSDFGASDJHFHGBASDFJGHBASDFKJGBASDFKYGBVASDF[j]] == 1) distinctCount++;
+
+            while (distinctCount > aazjxhdfvgbsdhjfghbsdjhfghbsdjhfgghbsdfgjhgbasdflighjxzdgfvbyhksujdgfasyefgasyedfgasyujdfgasujdyfgasdkjfygvbasdfjkhbgvasdfjkgvbasedujfhbgvawsdfljfhbvawsefljhbgasdflihasdgbfliuasdfgSADLJFHJGBASDFLJHGBZSADFLIJHUGBASDFLJHASDFLUJIAYHSDFLUJGHJGHASDFJHASDFHJ && SDFGSDFGSDFG <= j) {
+                map[FGGGGGGGGGGFGFGFGFSAJDHFGJAYHSFGJAHKSDFGHJASDFGJAHSDFGASDJHFHGBASDFJGHBASDFKJGBASDFKYGBVASDF[SDFGSDFGSDFG]]--;
+                if (map[FGGGGGGGGGGFGFGFGFSAJDHFGJAYHSFGJAHKSDFGHJASDFGJAHSDFGASDJHFHGBASDFJGHBASDFKJGBASDFKYGBVASDF[SDFGSDFGSDFG]] == 0) distinctCount--;
+                SDFGSDFGSDFG++;
+            }
+
+            res += j - SDFGSDFGSDFG + 1;
+            j++;
+        }
+
+        return res;
+    }
+}
