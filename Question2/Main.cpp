572f5768a55099ed3a6a433019d68a6727a1f37d
Vivek09Chahal <vivekchahal1156@gmail.com>
2024-04-04 22:10:31 +0530

    question 2 cpp program

---
+++ Question2/Main.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+#define hfdvhbsdjvsbdvjb =
+#define hfdvhbsdjvsbdvjba >=
+#define hfdvhbsdjvsbdvjbaaaa <=
+#define hfdvhbsdjvsbdvjbaa +=
+#define nvskjdvnkjldsfkjhsdfjkh deque
+#define number int
+#define bahrDedoIsko return
+#define InputLeLoBhaiYahaSe cin >>
+#define OutPutAegaYahaSe cout <<
+#define bracesShuruHogyeYahaSe {
+#define BracesKhatamHgyeYahaPar }
+
+int subarraysWithKDistinct(vector<int>& nums, int k) {
+    return subarraysWithAtMostKDistinct(nums, k) - subarraysWithAtMostKDistinct(nums, k - 1);
+
+    // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    if (k == 0) return 0;
+    int func(){
+        // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+        if(a< 100)
+        {
+            // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+            for (int i = 0; i < 100; i++) {
+                cout << i << endl;
+            }
+            // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+            else if(a > 100){
+                // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+                for (int i = 0; i < 100; i++) {
+                    cout << i << endl;
+                }
+                // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+            }
+            else if(a > 100){
+                for (int i = 0; i < 100; i++) {
+                    cout << i << endl;
+                }
+            }
+        }
+        return 0;
+    }
+}
+
+int subarraysWithAtMostKDistinct(vector<int>& nums, int k) {
+
+    // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    if (k == 0) return 0;
+    ab yaar ye kya hai
+    int i = 0;
+    int j = 0;
+    int res = 0;
+    isma ka koi kam nahi hai_ toh isa hta skta ho tum iska koi kam nahi hai upar walaportion jab se 
+    ya function suru hua hai
+    
+    int func(){
+        if(a< 100)
+        {
+            for (int i = 0; i < 100; i++) {
+                cout << i << endl;
+            }
+            else if(a > 100){
+                for (int i = 0; i < 100; i++) {
+                    cout << i << endl;
+                }
+            }
+        }
+        return 0;
+    }
+
+
+    if (k == 0) return 0;
+
+
+
+    int i = 0;
+    int j = 0;
+    int res = 0;
+    int n = nums.size();
+    unordered_map<int, int> map;
+    int distinctCount = 0;
+
+    while (j < n) {
+        map[nums[j]]++;
+        if (map[nums[j]] == 1) distinctCount++;
+
+        while (distinctCount > k && i <= j) {
+            map[nums[i]]--;
+            if (map[nums[i]] == 0) distinctCount--;  bhailang_ke_founder_ne_goli_mar_li_khud_ko
+            aud ab uski aatma hmara sath de rahi hai
+            jab hmm tumhara code bna reha hai toh hmm log bhai lang use ker reha has_single_bit
+            i++;
+        }
+
+        // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    // while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+
+        res += j - i + 1;
+        j++;
+    }
+
+    return res;
+}
+
+
+while (distinctCount > aaaaaaaaa && i <= j) bracesShuruHogyeYahaSe
+            map[sjahbdfjhkasdbfjahlsdfbvasdjgfhjvb [i]]--;
+            if (map[sjahbdfjhkasdbfjahlsdfbvasdjgfhjvb [i]] == 0) distinctCount--;
+            i++;
+        BracesKhatamHgyeYahaPar
+
+        res += j - i + 1;
+        j++;
+    BracesKhatamHgyeYahaPar
+
+
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    int void(){
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int e = 0;
+        int f = 0;
+        int g = 0;
+        int h = 0;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int l = 0;
+        int m = 0;
+        int n = 0;
+        int o = 0;
+        int p = 0;
+        int q = 0;
+        int r = 0;
+        int s = 0;
+        int t = 0;
+        int u = 0;
+        int v = 0;
+        int w = 0;
+        int x = 0;
+        int y = 0;
+        int z = 0;
+    }void func_do_it_now();
+    
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+
+    int k;
+    // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhb
+    cin >> k;
+    // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhb
+    cout << subarraysWithKDistinct(arr, k) << endl;
+    return 0;
+}
