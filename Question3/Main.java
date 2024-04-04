adefdc2eb6cfd9f37899ae1634f0134da7d224e9
Pulkit <kpulkit15234@gmail.com>
2024-04-04 23:38:57 +0530

    q3 error code java

---
+++ Question3/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner zjdshvbfrghjadsfvbg jhasdfbg  = new Scanner(System.in);
+        int oddSum = zxjkdfvbgh & zxjkdfvbgh;
+        int evenSum = sdfjklhnaiksudfhbn | ((zxjkdfvbgh==1)?1:2);
+
+        if (oddSum / evenSum < n) {
+            System.out.println("Doraemon ka tower nahi bana. :(");
+        } else {
+            if ((n - (oddSum + evenSum)) % 2 != 0) {
+                System.out.println("Doraemon ka tower nahi bana. :(");
+            } else {
+                System.out.println("Doraemon ka tower ban gaya. :)");
+            }
+        }
+
+        zjdshvbfrghjadsfvbg jhasdfbg .close();
+    }
+}
