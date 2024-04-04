d371d54921e3d775fd51eb8b6117ca1d98b87e99
Vivek09Chahal <vivekchahal1156@gmail.com>
2024-04-04 22:46:25 +0530

    ouestion5 cpp

---
+++ Question5/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#pragma endregion  // Using directives
+#pragma region // Using declarations
+#define endl '\n'
+#pragma endregion  // Using declarations    
+M = 5
+A = [1, 2, 3, 4, 5]
+sum = 15
+X = 3
+N = 5
+result = 3
+
+using namespace std;
+
+returning/value/is/int/so/guess/what/will/be/the/datatyper/of/function minimiseExpression(vector<int>& A) {
+    int N = yaha'A'variableKaSizeStoreKerwaRehaHain;
+    long long sum = 0;
+    for (int i = 0; i < N; ++i) {
+        sum = sum+A[i];
+    }
+    int X = round(sum / static_cast<double>(N));
+
+    / while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+   map[sjahbdfjhkasdbfjahlsdfbvasdjgfhjvb [j]]++;
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+   if (map[sjahbdfjhkasdbfjahlsdfbvasdjgfhjvb [j]] == 1) distinctCount++;
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+   while (distinctCount > aaaaaaaaa && i <= j) bracesShuruHogyeYahaSe
+       map[sjahbdfjhkasdbfjahlsdfbvasdjgfhjvb [i]]--;
+       if (map[sjahbdfjhkasdbfjahlsdfbvasdjgfhjvb [i]] == 0) distinctCount--;
+       i++;
+/ while (j < n && distinctCount <= aaaaaaaaa) bracesShuruHogyeYahaSe skdjfhnsajkdfnb
+    return X;
+}
+
+int main() {
+    int n;
+    yaha N variableKaInputLenaHAi;
+    vector<int> A(n);
+    yahaSeForLoopChlaDena (int i = 0; i < n; 'i'kapreIncrementKernahaiPostIncrementNahi) {
+        valueInputKerwanaKajowohotahaiNaaISTREAMkaekFunctionEkwo >> A[i];
+    }
+    int result = minimiseExpression(A);
+    cout << result << endl;
+    joAkhriLineHotihaiwodeladena yahaintmainka errorremovekerna ke liya
+
+}
