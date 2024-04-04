b4c89d74ebac69a864d432a0bdf296cc951c0303
Pulkit <kpulkit15234@gmail.com>
2024-04-04 23:40:40 +0530

    q8 added java

---
+++ Question8/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < n; i++) {
+            in.nextLine();
+            for ( {
+                for () {
+                    if () {
+                        count[j][k]++;
+                    }
+                }
+            }
+        }
+        for () {
+            for () {
+                if () {
+                    countPlanet++;
+                } else if () {
+                    countStar++;
+                }
+            }
+    }
+}
