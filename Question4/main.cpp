828decb45478686a9c052972fcb8f41e90002b70
Vivek09Chahal <vivekchahal1156@gmail.com>
2024-04-05 08:53:03 +0530

    '

---
+++ Question4/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+import java.util.Scanner;
+import java.util.Set;
+import java.util.ArrayList;
+using namespace std;
+#define hfdvhbsdjvsbdvjb =
+#define hfdvhbsdjvsbdvjba >=
+#define hfdvhbsdjvsbdvjbaaaa <=
+#define hfdvhbsdjvsbdvjbaa +=
+#define nvskjdvnkjldsfkjhsdfjkh deque
+#define number int
+#define bahrDedoIsko return
+#define InputLeLoBhaiYahaSe cin >>
+#define OutPutAegaYahaSe cout <<
+#define bracesShuruHogyeYahaSe {
+#define BracesKhatamHgyeYahaPar }
+
+using namespace std;
+
+return_Ki_Statement_kiTypeDekhKerIskaDataTypeLikhDo😂 minimiseExpression(vector<int>& A) {
+    int N = A.size();
+    long long sum = 0;
+>>>> sdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdn
+        // sdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdn
+        >>>>> sdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdn
+        // sdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdnjnjsdn
+    for (int i = 0; i < N; ++i) {
+        sum += A[i];
+        Scanner asbdfhbasjbfasjfbajsfdbbsdvdfhasvfhasfvhsafhasfvhsafasfhasfjhsdgasdghjasgdfjhasgdasfdgasgasfdgasdgas = new Scanner(System.in);
+    }
+    int X = round(sum / static_cast<double>(N));
+    YahaSeWapisBhejDoFirIsa X;
+}
+
+int main() {
+    int n;
+    YahaParInputLeLo >> n;
+    vector<int> A(n);
+     while (i <  sjsadfjaskasdfasdkbfaksjdfakjsfdkasdbfjasdbfjasbdkjasfbkjasdbaskd.length && j < njnajksdnasjkfnjkasnfdjasdnfkasndjknasfdknsafdknsadfnasdfkjnasdkfnajskdnfkasjfnaskdjnfjkasdnfkjasdnfjkasdfn.length) {
+            if ( sjsadfjaskasdfasdkbfaksjdfakjsfdkasdbfjasdbfjasbdkjasfbkjasdbaskd[i] < njnajksdnasjkfnjkasnfdjasdnfkasndjknasfdknsafdknsadfnasdfkjnasdkfnajskdnfkasjfnaskdjnfjkasdnfkjasdnfjkasdfn[j]) {
+                _____________________p____________________________jd[k++] =  sjsadfjaskasdfasdkbfaksjdfakjsfdkasdbfjasdbfjasbdkjasfbkjasdbaskd[i++];
+            } else {
+                _____________________p____________________________jd[k++] = njnajksdnasjkfnjkasnfdjasdnfkasndjknasfdknsafdknsadfnasdfkjnasdkfnajskdnfkasjfnaskdjnfjkasdnfkjasdnfjkasdfn[j++];
+            }
+     }
+        while (j < njnajksdnasjkfnjkasnfdjasdnfkasndjknasfdknsafdknsadfnasdfkjnasdkfnajskdnfkasjfnaskdjnfjkasdnfkjasdnfjkasdfn.length) {
+            _____________________p____________________________jd[k++] = njnajksdnasjkfnjkasnfdjasdnfkasndjknasfdknsafdknsadfnasdfkjnasdkfnajskdnfkasjfnaskdjnfjkasdnfkjasdnfjkasdfn[j++];
+        }
+        return _____________________p____________________________jd;
+    }
+    for (int i = 0; i < n; ++i) {
+        cin >> A[i];
+    }
+    int result = minimiseExpression(A);
+    while (i <  sjsadfjaskasdfasdkbfaksjdfakjsfdkasdbfjasdbfjasbdkjasfbkjasdbaskd.length) {
+            _____________________p____________________________jd[k++] =  sjsadfjaskasdfasdkbfaksjdfakjsfdkasdbfjasdbfjasbdkjasfbkjasdbaskd[i++];
+        }
+    PrintingResultHere << result << endl;
+    return 0;
+}
