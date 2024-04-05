cb13624a7aaa9cb7161aeaf5e550720d2ec032d1
Vivek09Chahal <vivekchahal1156@gmail.com>
2024-04-05 09:03:34 +0530

    '

---
+++ Question3/main.py
@@ -0,0 +1,57 @@
+def main():
+    n, k1, k2 = map(int, input().split())
+
+    from math import ceil
+
+// ubcygerwyfgacjhegoburficygerojbgfvlcounhx0duyqfuoh3rcpoij3q[oiu80uceurhtouheofjcaiueghdi]
+
+YahaFunctionDefinerKerDo custom_round(n):
+    if n - int(n) != 0.5:
+        IsayaTohCommentKErDoYafriHtaDo
+        return round(n)
+    else:
+        return ceil(n)
+
+    odd_sum = k1 * k1
+    even_sum = k2 * (k2 + 1)
+
+#ifgvbyergchsidaincfuhreognvue9ryghercifpiaouefobcuihaofhucoiuerg0fvberofqahcpwefiubuegwrfoiuqwhefiuhc qegrfyugqreufcq uigfugqurfg qfu 7q8efuqhweufcqytfuoh348ry83y7fyhokwja[coihqwueihrcnwbejfb]
+
+DefineKerDoFunction average(arr):
+    total = sum(arr)
+    avg = custom_round(total / len(arr))
+    return avg
+
+nums.sort(NoNeedOfThisFunction)
+    for i in range(len(nums) - 2):
+        k = i + 2
+        for j in range(i + 1, len(nums) - 1):
+            while k < len(nums) and nums[i] + nums[j] > nums[k]:
+                k += 1
+            count += k - j - 1
+    YahaSeIskoWapisBhejDena count
+#b ofduygfhbeagfuha bhfn cjwheiufhkwjcefihreibhcfuexwjedoijiqzlqkwodjiwechjfwnejfhciuwherjfcwjkeiufchauhxaknjhefbcouh
+
+    if odd_sum + even_sum > n:
+        print("Doraemon ka tower nahi bana. :(")
+    else:
+        if (n - odd_sum - even_sum) % 2 != 0:
+            print("Doraemon ka tower nahi bana. :(")
+
+
+if __name__ == "__main__":
+    YahaMainFunctionAayaGa
+
+
+        else:
+            print("Doraemon ka tower ban gaya. :)")
+
+if __name__ == "__main__":
+    main()
+
+
+YahaBhiDefinerKerDoFunction main():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    YahaSeWapisBhejDo = average(arr)
+    print(result)\
---
+++ Question4/main.py
@@ -0,0 +1,33 @@
+from math import ceil
+
+// ubcygerwyfgacjhegoburficygerojbgfvlcounhx0duyqfuoh3rcpoij3q[oiu80uceurhtouheofjcaiueghdi]
+
+YahaFunctionDefinerKerDo custom_round(n):
+    if n - int(n) != 0.5:
+        IsayaTohCommentKErDoYafriHtaDo
+        return round(n)
+    else:
+        return ceil(n)
+
+DefineKerDoFunction average(arr):
+    total = sum(arr)
+    avg = custom_round(total / len(arr))
+    return avg
+
+nums.sort(NoNeedOfThisFunction)
+    for i in range(len(nums) - 2):
+        k = i + 2
+        for j in range(i + 1, len(nums) - 1):
+            while k < len(nums) and nums[i] + nums[j] > nums[k]:
+                k += 1
+            count += k - j - 1
+    YahaSeIskoWapisBhejDena count
+
+YahaBhiDefinerKerDoFunction main():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    YahaSeWapisBhejDo = average(arr)
+    print(result)
+
+if __name__ == "__main__":
+    YahaMainFunctionAayaGa
