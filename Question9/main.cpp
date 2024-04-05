fb955e0b3988372d532b3b3b5e435205c8e97ec9
Vivek09Chahal <vivekchahal1156@gmail.com>
2024-04-05 11:20:13 +0530

    '

--- Question9/main.cpp
+++ Question9/main.cpp
@@ -1,63 +1,45 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
+using namespace std;
 #define hfdvhbsdjvsbdvjb =
 #define hfdvhbsdjvsbdvjba >=
 #define hfdvhbsdjvsbdvjbaaaa <=
 #define hfdvhbsdjvsbdvjbaa +=
 #define nvskjdvnkjldsfkjhsdfjkh deque
 #define number int
-
-// while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
-int main() {
-    int n, r, c;
-    YahaInpputLenaHAi >> n >> r >> c;
-
-    vector<vector<int>> count(r, vector<int>(c, 0));
-    int twinker_twinkel_littel_start_how_are_are_when_countStar = 0;
-    int countPlanet = 0;
-
-isma ka koi kam nahi hai_ toh isa hta skta ho tum iska koi kam nahi hai upar walaportion jab se 
-    ya function suru hua hai
 #define bahrDedoIsko return
 #define InputLeLoBhaiYahaSe cin >>
 #define OutPutAegaYahaSe cout <<
 #define bracesShuruHogyeYahaSe {
 #define BracesKhatamHgyeYahaPar }
-// while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
-    for (int i = 0; i < n; i++) {
-        string image;
-        cin >> image;
-        cin.ignore();
 
-        LoopLagaDoAbKonsaLaganaHAiWoDekhLena (int j = 0; j < r; j++) {
-            string line;
-            getline(cin, line);
-// while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
-            foLoopLagaDoAbKonsaLaganaHAiWoDekhLena r (int k = 0; k < c; k++) {
-                if (line[k] == '*') {
-                    count[j][k]++;
-                }
-            }
-        }
+int subarraysWithKDistinct(vector<int>& nums, int k) {
+    return subarraysWithAtMostKDistinct(nums, k) - subarraysWithAtMostKDistinct(nums, k - 1);
+
+    // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    if (k == 0) return 0;
+    int func(){
+        // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+        if(a< 100)
     }
 
-    LoopLagaDoAbKonsaLaganaHAiWoDekhLena  (int i = 0; i < r; i++) {
-        LoopLagaDoAbKonsaLaganaHAiWoDekhLena  (int j = 0; j < c; j++) {
-            // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
-            if (count[i][j] == n) {
-                countPlanet++;
-            } else if (count[i][j] != 0) {
-                countStar++;
-                // while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
-            }
-        }
+int main() {
+    int n;
+    yahaInputKLlo >> n;
+
+    int bitLen = 0;
+    int temp = n;
+    while (temp > 0) {
+        bitLen++;
+        temp >>= 1;
     }
 
-    cout << countPlanet << " " << countStar << endl;
-// while func() bracesShuruHogyeYahaSe  skdjfhnsajkdfnb zdfjhsdfjkhbzdfjhsdfjkhbzdfjhsdfjkhb
+    if (n > 0 && ((n & (n - 1)) == 0) && (bitLen % 2 == 1)) {
+        cout << "Char Charizard!!" << endl;
+    } else {
+        cout << "Char Charizard nahi!!" << endl;
+    }
+
     return 0;
 }
--- Question9/main.py
+++ Question9/main.py
@@ -1,5 +1,5 @@
 def main():
-    n, r, c = MapDefinerKerDo(int, input().split())
+    n, r, c = map(int, input().split())
     count = [[0 for _ in range(c)] for _ in range(r)]
     count_star = 0
     count_planet = 0
@@ -12,34 +12,14 @@ def main():
                 if line[k] == '*':
                     count[j][k] += 1
 
-    if __name__ == "__main__":
-    n = int(input())
-
-     // Function to find the number of subarrays with at most k distinct elements
-    // in an array
-
     for i in range(r):
         for j in range(c):
             if count[i][j] == n:
                 count_planet += 1
             elif count[i][j] != 0:
                 count_star += 1
-    
-    # Time complexity: O(n)
-    # Space complexity: O(k)
-    
-    arr = list(map(int, input().split()))
-    print(triangle_number(arr))
 
-    IsaPrintKerwanaHai(count_planet, count_star)
+    print(count_planet, count_star)
 
 if __name__ == "__main__":
     main()
-
-yahaWhaiJOTumFunctionSeUltaBhejtaHo(YES<return) avg
-
-DefineMainFunctionHere main():
-    n = int(input())
-    arr = list(map(int, input().split()))
-    result = average(arr)
-    print(result)
